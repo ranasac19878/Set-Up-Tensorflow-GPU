0d1f9cacf05eaf0993fd6ff478a06612bd172b0d
ranasac19878 <s.rana@dessa.com>
2019-01-04 16:06:20 -0500

    split in two

--- script_1.sh
+++ script_1.sh
@@ -50,17 +50,3 @@ wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/cuda-repo-ubun
 
 sudo dpkg -i cuda-repo-ubuntu1604-9-0-local-cublas-performance-update-2_1.0-1_amd64-deb
 
-tar -xzvf ~/Set-Up-Tensorflow-GPU/cudnn-9.0-linux-x64-v7.1.tgz
-sudo cp cuda/include/cudnn.h /usr/local/cuda/include
-sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
-sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
-
-conda create -n tf python=3.6 pip
-source activate tf
-pip install --upgrade tensorflow-gpu==1.12.0
-
-
-
-
-
-sudo reboot
---
+++ script_2.sh
@@ -0,0 +1,14 @@
+tar -xzvf ~/Set-Up-Tensorflow-GPU/cudnn-9.0-linux-x64-v7.1.tgz
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+
+conda create -n tf python=3.6 pip
+source activate tf
+pip install --upgrade tensorflow-gpu==1.12.0
+
+
+
+
+
+sudo reboot
