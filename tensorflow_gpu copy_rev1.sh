7487bb151ef8201b4f52fb68680042c64610fd2a
ranasac19878 <s.rana@dessa.com>
2019-01-04 13:47:01 -0500

    initial commit

---
+++ tensorflow_gpu copy_rev1.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+DEBIAN_FRONTEND=noninteractive
+
+sudo rm /etc/apt/apt.conf.d/*.*
+sudo apt update
+mkdir downloads
+cd downloads
+wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+bash Anaconda3-5.0.1-Linux-x86_64.sh -b
+cd
+echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
+export PATH=~/anaconda3/bin:$PATH
+source ~/.bashrc
+sudo apt install unzip -y
+sudo apt -y upgrade --force-yes
+sudo apt -y autoremove
+jupyter notebook --generate-config
+echo "c.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
+sudo ufw allow 8888/tcp
+sudo apt -y install qtdeclarative5-dev qml-module-qtquick-controls
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo apt update
+pip install ipywidgets
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
+
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt install nvidia-390
+
+sudo nvidia-smi
+
+cd
+cd downloads
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
+
+echo 'export PATH=/usr/local/cuda-9.0/bin${PATH:+:$PATH}}' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/cuda-repo-ubuntu1604-9-0-local-cublas-performance-update_1.0-1_amd64-deb
+
+sudo dpkg -i cuda-repo-ubuntu1604-9-0-local-cublas-performance-update_1.0-1_amd64-deb
+
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/cuda-repo-ubuntu1604-9-0-local-cublas-performance-update-2_1.0-1_amd64-deb
+
+sudo dpkg -i cuda-repo-ubuntu1604-9-0-local-cublas-performance-update-2_1.0-1_amd64-deb
+
+tar -xzvf cudnn-9.0-linux-x64-v7.1.tgz
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+
+conda create -n tf python=3.6 pip
+source activate tf
+pip install --upgrade tensorflow-gpu==1.12.0
+
+
+
+
+
+sudo reboot
